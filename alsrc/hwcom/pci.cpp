a534893b63555f5ed1a48755656b9b9393a00081
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2018-06-11 05:49:29 +0200

    Create pci.cpp

---
+++ alsrc/hwcom/pci.cpp
@@ -0,0 +1,197 @@
+/*
+ * This file is part of Asea OS.
+ * Copyright (C) 2018 Ivan Kmeťo
+ *
+ * Asea OS is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Asea OS is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Asea OS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <hwcom/pci.h>
+
+using namespace asea::common;
+using namespace asea::drivers;
+using namespace asea::hwcom;
+
+PCInterconnectDeviceDescriptor::PCInterconnectDeviceDescriptor() {
+}
+
+PCInterconnectDeviceDescriptor::~PCInterconnectDeviceDescriptor() {
+}
+
+PCInterconnectController::PCInterconnectController()
+: dataPort(0xCFC),
+commandPort(0xCF8) {
+}
+
+PCInterconnectController::~PCInterconnectController() {
+}
+
+uint32_t PCInterconnectController::Read(uint16_t bus, uint16_t device, uint16_t function, uint32_t registeroffset) {
+	uint32_t id =
+	0x1 << 31
+	| ((bus & 0xFF) << 16)
+	| ((device & 0x1F) << 11)
+	| ((function & 0x07) << 8)
+	| (registeroffset & 0xFC);
+
+	commandPort.Write(id);
+	uint32_t result = dataPort.Read();
+
+	return result >> (8* (registeroffset % 4));
+}
+
+void PCInterconnectController::Write(uint16_t bus, uint16_t device, uint16_t function, uint32_t registeroffset, uint32_t value) {
+	uint32_t id =
+	0x1 << 31
+	| ((bus & 0xFF) << 16)
+	| ((device & 0x1F) << 11)
+	| ((function & 0x07) << 8)
+	| (registeroffset & 0xFC);
+
+	commandPort.Write(id);
+	dataPort.Write(value);
+}
+
+bool PCInterconnectController::DeviceHasFunctions(uint16_t bus, uint16_t device){
+	return Read(bus, device, 0, 0x0E) & (1<<7);
+}
+
+void printf(char* str);
+void printfHex(uint8_t);
+
+void PCInterconnectController::SelectDrivers(DriverManager* driverManager, InterruptManager* interrupts) {
+	for(int bus = 0; bus < 8; bus++)
+	{
+		for(int device = 0; device < 32; device++)
+		{
+			int numFunctions = DeviceHasFunctions(bus, device) ? 8 : 1;
+			for(int function =0; function < numFunctions; function++)
+			{
+				PCInterconnectDeviceDescriptor dev = GetDeviceDescriptor(bus, device, function);
+
+				if(dev.vendor_id == 0x0000 || dev.vendor_id == 0xFFFF)
+					continue;
+
+				for(int barNum = 0; barNum < 6; barNum++)
+				{
+					BaseAddressRegister bar = GetBaseAddressRegister(bus, device, function, barNum);
+					if(bar.address && (bar.type == InputOutput))
+						dev.portBase = (uint32_t)bar.address;
+
+					Driver* driver = GetDriver(dev, interrupts);
+					if(driver != 0)
+						driverManager->AddDriver(driver);
+				}
+
+				printf("PCI BUS ");
+				printfHex(bus & 0xFF);
+
+				printf(", DEVICE ");
+				printfHex(device & 0xFF);
+
+				printf(" = VENDOR ");
+				printfHex((dev.vendor_id & 0xFF00) >> 8);
+				printfHex(dev.vendor_id & 0xFF);
+
+				printf(", DEVICE ");
+				printfHex((dev.device_id & 0xFF00) >> 8);
+				printfHex(dev.device_id & 0xFF);
+				printf("\n");
+			}
+		}
+	}
+}
+
+BaseAddressRegister PCInterconnectController::GetBaseAddressRegister(uint16_t bus, uint16_t device, uint16_t function, uint16_t bar) {
+	BaseAddressRegister result;
+
+	uint32_t headertype = Read(bus, device, function, 0x0E) & 0x7F;
+	int maxBARs = 6 - (4*headertype);
+	if(bar >= maxBARs)
+		return result;
+
+	uint32_t bar_value = Read(bus, device, function, 0x10 + 4*bar);
+	result.type = (bar_value & 0x1) ? InputOutput : MemoryMapping;
+	uint32_t temp;
+
+	if(result.type == MemoryMapping)
+	{
+		switch((bar_value >> 1) & 0x3)
+		{
+			case 0: //32Bit Mode
+			case 1: //20Bit Mode
+			case 2: //64Bit Mode
+				break;
+		}
+		//result.prefetchable = ((bar_value >> 3) & 0x1) == 0x1;
+	}
+	else //InputOutput
+	{
+		result.address = (uint8_t*)(bar_value & ~0x3);
+		result.prefetchable = false;
+	}
+
+	return result;
+}
+
+Driver* PCInterconnectController::GetDriver(PCInterconnectDeviceDescriptor dev, InterruptManager* interrupts) {
+	switch(dev.vendor_id)
+	{
+		case 0x1022: // AMD
+			switch(dev.device_id)
+			{
+				case 0x2000: //am79c973 (driver?)
+					break;
+			}
+			break;
+
+		case 0x8086: // Intel
+			break;
+	}
+
+	//THIS SHOULD BE IN SEPARATE FILE ON DISK CONTAINING DRIVER
+	switch(dev.class_id)
+	{
+		case 0x03: //Graphics, Generic Devices
+			switch(dev.subclass_id)
+			{
+				case 0x00: //VGA
+					//printf("VGA");
+					break;
+			}
+			break;
+	}
+	//END SHOULD BE IN SEPARATE FILE ON DISK CONTAINING DRIVER
+
+	return 0;
+}
+
+PCInterconnectDeviceDescriptor PCInterconnectController::GetDeviceDescriptor(uint16_t bus, uint16_t device, uint16_t function) {
+	PCInterconnectDeviceDescriptor result;
+
+	result.bus = bus;
+	result.device = device;
+	result.function = function;
+
+	result.vendor_id = Read(bus, device, function, 0x00);
+	result.device_id = Read(bus, device, function, 0x02);
+
+	result.class_id = Read(bus, device, function, 0x0b);
+	result.subclass_id = Read(bus, device, function, 0x0a);
+	result.interface_id = Read(bus, device, function, 0x09);
+
+	result.revision = Read(bus, device, function, 0x08);
+	result.interrupt = Read(bus, device, function, 0x3c);
+
+	return result;
+}
