8a7592324535cef197ee9b0347a065ffb024182c
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2018-06-19 05:43:22 +0200

    Create verdef.h

---
+++ alinc/System/headers/verdef.h
@@ -0,0 +1,26 @@
+/*
+ * This file is part of Asea OS.
+ * Copyright (C) 2018 Ivan Kmeťo
+ *
+ * Asea OS is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Asea OS is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Asea OS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ASEA__SYSTEM__HEADERS__VERDEF_H
+#define __ASEA__SYSTEM__HEADERS__VERDEF_H
+
+#define ASD_AL_VERSION "0.1.9"
+#define ASD_AL_BUILDVR "(build dev180619)"
+#define ASD_AS_VERSION "0.1"
+
+#endif
