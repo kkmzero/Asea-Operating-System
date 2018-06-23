0c5a801f5ef149e7bfb7c8c11b3f1122957a1737
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2018-06-23 07:42:46 +0200

    Update driver.h

--- alinc/drivers/driver.h
+++ alinc/drivers/driver.h
@@ -25,26 +25,26 @@ namespace asea
 	{
 		class Driver
 		{
-			public:
-				Driver();
-				~Driver();
+		public:
+			Driver();
+			~Driver();
 
-				virtual void Activate();
-				virtual int Reset();
-				virtual void Deactivate();
+			virtual void Activate();
+			virtual int Reset();
+			virtual void Deactivate();
 		};
 
 		class DriverManager
 		{
-			private:
-				Driver* drivers[265];
-				int numDrivers;
+		private:
+			Driver* drivers[265];
+			int numDrivers;
 
-			public:
-				DriverManager();
-				void AddDriver(Driver*);
+		public:
+			DriverManager();
+			void AddDriver(Driver*);
 
-				void ActivateAll();
+			void ActivateAll();
 		};
 
 	}
