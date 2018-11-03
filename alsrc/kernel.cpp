c3a2756db9023afdd06f1d66c1c07eb3469f25a5
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2018-11-03 07:45:58 +0100

    Update kernel.cpp
    Implemented \b Escape sequence in printf (kernel.cpp)

--- alsrc/kernel.cpp
+++ alsrc/kernel.cpp
@@ -34,7 +34,7 @@ using namespace asea::System::headers;
 
 void printf(char* str) {
 	static uint16_t* VideoMemory = (uint16_t*)0xb8000;
-	static uint8_t x =0, y=0;
+	static uint8_t x = 0, y = 0;
 
 	for(int i = 0; str[i] != '\0'; ++i)
 	{
@@ -43,6 +43,17 @@ void printf(char* str) {
 			case '\n':
 				y++;
 				x = 0;
+				break;
+
+			case '\b':
+				if (x <= 0)
+				{
+					x = 0;
+				}
+				else {
+					x -= 1;
+				}
+				VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
 				break;
 
 			default:
