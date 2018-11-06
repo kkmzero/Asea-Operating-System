53d291dafc49b66483c309d6bde8a44ea53b101f
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2018-11-06 10:42:03 +0100

    Update kernel.cpp
    - Moved printfHex to Asea System Library (kernel.cpp, interrupts.cpp, pci.cpp, keyboard.cpp, asyslib.cpp, asyslib.h)

--- alsrc/kernel.cpp
+++ alsrc/kernel.cpp
@@ -80,14 +80,6 @@ void printf(char* str) {
 	}
 }
 
-void printfHex(uint8_t key) {
-	char* foo = "00";
-	char* hex = "0123456789ABCDEF";
-	foo[0] = hex[(key >> 4) & 0xF];
-	foo[1] = hex[key & 0xF];
-	printf(foo);
-}
-
 class PrintfKeyboardEventHandler : public KeyboardEventHandler
 {
 	public:
