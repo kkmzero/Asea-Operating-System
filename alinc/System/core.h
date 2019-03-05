e380f4221c1eaadc2c92696f4fac86612980deff
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2019-03-05 20:42:18 +0100

    Update core.h

--- alinc/System/core.h
+++ alinc/System/core.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of Asea OS.
- * Copyright (C) 2018 Ivan Kmeťo
+ * Copyright (C) 2018, 2019 Ivan Kmeťo
  *
  * Asea OS is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
