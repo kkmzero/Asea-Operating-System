f60c3a8328d3c1380d35695804a72beecfedd4f8
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2019-05-22 14:50:31 +0200

    Create asl.h

---
+++ alinc/System/lib/asl.h
@@ -0,0 +1,25 @@
+/*
+ * This file is part of Asea OS.
+ * Copyright (C) 2018 - 2019 Ivan Kmeťo
+ *
+ * Asea OS is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Asea OS is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Asea OS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ASEA__SYSTEM__LIB__ASL_H
+#define __ASEA__SYSTEM__LIB__ASL_H
+
+#include "asl/corefunc.h"
+#include "asl/io.h"
+
+#endif
