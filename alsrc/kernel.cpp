7f2d97067d7eeaa7679496964b3ffed657f1234d
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2019-06-29 12:46:28 +0200

    Update kernel.cpp

--- alsrc/kernel.cpp
+++ alsrc/kernel.cpp
@@ -24,7 +24,7 @@
 #include <drivers/driver.h>
 #include <drivers/keyboard.h>
 #include <drivers/mouse.h>
-#include <drivers/vga.h>
+//#include <drivers/vga.h>
 #include <System/headers/sysnfo.h>
 #include <System/headers/sysmsgs.h>
 #include <System/lib/asl.h>
@@ -40,182 +40,182 @@ using namespace asea::System::core;
 
 
 void cursor_enable(uint8_t cursor_start, uint8_t cursor_end) {
-	asl::io::outb(0x3D4, 0x0A);
-	asl::io::outb(0x3D5, (asl::io::inb(0x3D5) & 0xC0) | cursor_start);
-	asl::io::outb(0x3D4, 0x0B);
-	asl::io::outb(0x3D5, (asl::io::inb(0x3D5) & 0xE0) | cursor_end);
+    asl::io::outb(0x3D4, 0x0A);
+    asl::io::outb(0x3D5, (asl::io::inb(0x3D5) & 0xC0) | cursor_start);
+    asl::io::outb(0x3D4, 0x0B);
+    asl::io::outb(0x3D5, (asl::io::inb(0x3D5) & 0xE0) | cursor_end);
 }
 
 void cursor_disable() {
-	asl::io::outb(0x3D4, 0x0A);
-	asl::io::outb(0x3D5, 0x20);
+    asl::io::outb(0x3D4, 0x0A);
+    asl::io::outb(0x3D5, 0x20);
 }
 
 void cursor_update(int32_t x, int32_t y) {
-	uint16_t pos = y * 80 + x;
- 
-	asl::io::outb(0x3D4, 0x0F);
-	asl::io::outb(0x3D5, (uint8_t) (pos & 0xFF));
-	asl::io::outb(0x3D4, 0x0E);
-	asl::io::outb(0x3D5, (uint8_t) ((pos >> 8) & 0xFF));
+    uint16_t pos = y * 80 + x;
+
+    asl::io::outb(0x3D4, 0x0F);
+    asl::io::outb(0x3D5, (uint8_t) (pos & 0xFF));
+    asl::io::outb(0x3D4, 0x0E);
+    asl::io::outb(0x3D5, (uint8_t) ((pos >> 8) & 0xFF));
 }
 
 void printf(char* str) {
-	static uint16_t* VideoMemory = (uint16_t*)0xb8000;
-	static uint8_t x = 0, y = 0;
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+    static uint8_t x = 0, y = 0;
 
-	for(int i = 0; str[i] != '\0'; ++i)
-	{
-		switch(str[i])
-		{
-			case '\n':
-				y++;
-				x = 0;
-				break;
+    for(int i = 0; str[i] != '\0'; ++i)
+    {
+        switch(str[i])
+        {
+            case '\n':
+                y++;
+                x = 0;
+                break;
 
-			case '\b':
-				if (x <= 0)
-				{
-					x = 0;
-				}
-				else {
-					x -= 1;
-				}
-				VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
-				break;
+            case '\b':
+                if (x <= 0)
+                {
+                    x = 0;
+                }
+                else {
+                    x -= 1;
+                }
+                VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
+                break;
 
-			default:
-				VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | str[i];
-				x++;
-				break;
-		}
+            default:
+                VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | str[i];
+                x++;
+                break;
+        }
 
-		if(x >= 80)
-		{
-			y++;
-			x = 0;
-		}
+        if(x >= 80)
+        {
+            y++;
+            x = 0;
+        }
 
-		if (y >= 25)
-		{
-			for(y = 0; y < 25; y++)
-			for (x =0; x < 80; x++)
-			VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
+        if (y >= 25)
+        {
+            for(y = 0; y < 25; y++)
+            for (x =0; x < 80; x++)
+            VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
 
-			x = 0;
-			y = 0;
-		}
-	}
-	cursor_update(x, y);
+            x = 0;
+            y = 0;
+        }
+    }
+    cursor_update(x, y);
 }
 
 class PrintfKeyboardEventHandler : public KeyboardEventHandler
 {
-	public:
-		void OnKeyDown(char c)
-		{
-			char* foo = " ";
-			foo[0] = c;
-			printf(foo);
-		}
+    public:
+        void OnKeyDown(char c)
+        {
+            char* foo = " ";
+            foo[0] = c;
+            printf(foo);
+        }
 };
 
 class MouseToConsole : public MouseEventHandler
 {
-		int8_t x, y;
-	public:
-		MouseToConsole()
-		{
-			static uint16_t* VideoMemory = (uint16_t*)0xb8000;
-			x = 40;
-			y = 12;
-			VideoMemory[80*y+x] = ((VideoMemory[80*y+x] & 0xF000) >> 4)
-								| ((VideoMemory[80*y+x] & 0x0F00) << 4)
-								| ((VideoMemory[80*y+x] & 0x00FF));
-		}
+        int8_t x, y;
+    public:
+        MouseToConsole()
+        {
+            static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+            x = 40;
+            y = 12;
+            VideoMemory[80*y+x] = ((VideoMemory[80*y+x] & 0xF000) >> 4)
+                                | ((VideoMemory[80*y+x] & 0x0F00) << 4)
+                                | ((VideoMemory[80*y+x] & 0x00FF));
+        }
 
-	void OnMouseMove(int xoffset, int yoffset)
-	{
-		static uint16_t* VideoMemory = (uint16_t*)0xb8000;
-		VideoMemory[80*y+x] = ((VideoMemory[80*y+x] & 0xF000) >> 4)
-							| ((VideoMemory[80*y+x] & 0x0F00) << 4)
-							| ((VideoMemory[80*y+x] & 0x00FF));
+    void OnMouseMove(int xoffset, int yoffset)
+    {
+        static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+        VideoMemory[80*y+x] = ((VideoMemory[80*y+x] & 0xF000) >> 4)
+                            | ((VideoMemory[80*y+x] & 0x0F00) << 4)
+                            | ((VideoMemory[80*y+x] & 0x00FF));
 
-		x += xoffset;
-		if(x >= 80) x = 79;
-		if(x < 0) x = 0;
+        x += xoffset;
+        if(x >= 80) x = 79;
+        if(x < 0) x = 0;
 
-		y -= yoffset;
-		if(y >= 25) y = 24;
-		if(y < 0) y = 0;
+        y -= yoffset;
+        if(y >= 25) y = 24;
+        if(y < 0) y = 0;
 
-		VideoMemory[80*y+x] = ((VideoMemory[80*y+x] & 0xF000) >> 4)
-							| ((VideoMemory[80*y+x] & 0x0F00) << 4)
-							| ((VideoMemory[80*y+x] & 0x00FF));
-	}
+        VideoMemory[80*y+x] = ((VideoMemory[80*y+x] & 0xF000) >> 4)
+                            | ((VideoMemory[80*y+x] & 0x0F00) << 4)
+                            | ((VideoMemory[80*y+x] & 0x00FF));
+    }
 };
 
 typedef void (*constructor)();
 extern "C" constructor start_ctors;
 extern "C" constructor end_ctors;
 extern "C" void callConstructors() {
-	for(constructor* i = &start_ctors; i != &end_ctors; i++)
-		(*i)();
+    for(constructor* i = &start_ctors; i != &end_ctors; i++)
+        (*i)();
 }
 
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot_magic*/)
 {
-	cursor_disable();
-	cursor_enable(0, 15);
-	cursor_update(0, 0);
+    cursor_disable();
+    cursor_enable(0, 15);
+    cursor_update(0, 0);
 
-	AseaSystemInfo sysInfo;
-	AseaSystemMessages sysMsgs;
+    AseaSystemInfo sysInfo;
+    AseaSystemMessages sysMsgs;
 
-	sysInfo.AS_PrintSysInfoMsg(SYSINFOMSG_COPYRIGHTVR);
+    sysInfo.AS_PrintSysInfoMsg(SYSINFOMSG_COPYRIGHTVR);
 
-	GlobalDescriptorTable gdt;
-	InterruptManager interrupts(&gdt);
+    GlobalDescriptorTable gdt;
+    InterruptManager interrupts(&gdt);
 
-	/*
-	uint32_t* memupper = (uint32_t*)(((size_t)multiboot_structure) + 8);
-	size_t heap = 10*1024*1024;
-	MemMgr memMgr(heap, (*memupper)*1024 - heap - 10*1024);
-	
-	printf("heap: 0x");
-	asl::printfHex((heap >> 24) & 0xFF);
-	asl::printfHex((heap >> 16) & 0xFF);
-	asl::printfHex((heap >> 8 ) & 0xFF);
-	asl::printfHex((heap      ) & 0xFF);
-	void* allocated = memMgr.malloc(1024);
+    /*
+    uint32_t* memupper = (uint32_t*)(((size_t)multiboot_structure) + 8);
+    size_t heap = 10*1024*1024;
+    MemMgr memMgr(heap, (*memupper)*1024 - heap - 10*1024);
 
-	printf("\nallocated: 0x");
-	asl::printfHex(((size_t)allocated >> 24) & 0xFF);
-	asl::printfHex(((size_t)allocated >> 16) & 0xFF);
-	asl::printfHex(((size_t)allocated >> 8 ) & 0xFF);
-	asl::printfHex(((size_t)allocated      ) & 0xFF);
-	printf("\n\n");
-	*/
+    printf("heap: 0x");
+    asl::printfHex((heap >> 24) & 0xFF);
+    asl::printfHex((heap >> 16) & 0xFF);
+    asl::printfHex((heap >> 8 ) & 0xFF);
+    asl::printfHex((heap      ) & 0xFF);
+    void* allocated = memMgr.malloc(1024);
 
-	sysMsgs.AS_StatusMsgInf(STATUSMSG_INF_INITIALIZING);
-	DriverManager drvManager;
+    printf("\nallocated: 0x");
+    asl::printfHex(((size_t)allocated >> 24) & 0xFF);
+    asl::printfHex(((size_t)allocated >> 16) & 0xFF);
+    asl::printfHex(((size_t)allocated >> 8 ) & 0xFF);
+    asl::printfHex(((size_t)allocated      ) & 0xFF);
+    printf("\n\n");
+    */
 
-	PrintfKeyboardEventHandler kbhandler;
-	KeyboardDriver keyboard(&interrupts, &kbhandler);
-	drvManager.AddDriver(&keyboard);
+    sysMsgs.AS_StatusMsgInf(STATUSMSG_INF_INITIALIZING);
+    DriverManager drvManager;
 
-	MouseToConsole mousehandler;
-	MouseDriver mouse(&interrupts, &mousehandler);
-	drvManager.AddDriver(&mouse);
+    PrintfKeyboardEventHandler kbhandler;
+    KeyboardDriver keyboard(&interrupts, &kbhandler);
+    drvManager.AddDriver(&keyboard);
 
-	PCInterconnectController PCIController;
-	PCIController.SelectDrivers(&drvManager, &interrupts);
+    MouseToConsole mousehandler;
+    MouseDriver mouse(&interrupts, &mousehandler);
+    drvManager.AddDriver(&mouse);
 
-	drvManager.ActivateAll();
-	sysMsgs.AS_StatusMsg(STATUSMSG_OK, "Hardware Initialization\n");
+    PCInterconnectController PCIController;
+    PCIController.SelectDrivers(&drvManager, &interrupts);
 
-	interrupts.Activate();
-	sysMsgs.AS_StatusMsg(STATUSMSG_OK, "Hardware Interrupts\n\n");
+    drvManager.ActivateAll();
+    sysMsgs.AS_StatusMsg(STATUSMSG_OK, "Hardware Initialization\n");
 
-	while(1);
+    interrupts.Activate();
+    sysMsgs.AS_StatusMsg(STATUSMSG_OK, "Hardware Interrupts\n\n");
+
+    while(1);
 }
