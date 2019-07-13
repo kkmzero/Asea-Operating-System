d842d858bf3c809903ce0c93811dbf6fac5e93e3
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2019-07-13 19:54:31 +0200

    Create putchar.h

---
+++ lib/libastd/putchar.h
@@ -0,0 +1,34 @@
+/*
+ * This file is part of Asea OS.
+ * Copyright (C) 2019 Ivan Kmeťo
+ *
+ * Asea OS is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Asea OS is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Asea OS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ASEA__LIB__ASTD__PUTCHAR_H
+#define __ASEA__LIB__ASTD__PUTCHAR_H
+
+void printf(char*);
+
+
+//
+void putchar(char c)
+{
+    char* f = " ";
+    f[0] = c;
+    printf(f);
+}
+
+
+#endif
