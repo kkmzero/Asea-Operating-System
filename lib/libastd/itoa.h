5cf0e918525516df39215415253c37869d1d6543
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2019-07-13 19:55:24 +0200

    Create itoa.h

---
+++ lib/libastd/itoa.h
@@ -0,0 +1,51 @@
+/*
+ * This file is part of Asea OS.
+ * Copyright (C) 2019 Ivan Kmeťo
+ *
+ * Asea OS is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Asea OS is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Asea OS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ASEA__LIB__ASTD__ITOA_H
+#define __ASEA__LIB__ASTD__ITOA_H
+
+#include <common/types.h>
+
+
+//
+char* itoa(asea::common::int32_t value, char* result, asea::common::int32_t base)
+{
+    if (base < 2 || base > 36) { *result = '\0'; return result; }
+
+    char* ptr = result, *ptr1 = result, tmp_char;
+    int tmp_value;
+
+    do {
+        tmp_value = value;
+        value /= base;
+        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+    } while (value);
+
+    if (tmp_value < 0) *ptr++ = '-';
+    *ptr-- = '\0';
+    while(ptr1 < ptr) {
+        tmp_char = *ptr;
+        *ptr--= *ptr1;
+        *ptr1++ = tmp_char;
+    }
+
+    return result;
+}
+
+
+#endif
