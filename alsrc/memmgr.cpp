0b7b96bc760a08d0512b41651738ce026cd7d663
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2019-07-13 18:56:12 +0200

    formatting

--- alsrc/memmgr.cpp
+++ alsrc/memmgr.cpp
@@ -25,77 +25,77 @@ MemMgr* MemMgr::activeMemMgr = 0;
 
 MemMgr::MemMgr(size_t start, size_t size)
 {
-	activeMemMgr = this;
+    activeMemMgr = this;
 
-	if(size < sizeof(MemoryChunk)) {
-		first = 0;
-	}
-	else {
-		first = (MemoryChunk*)start;
+    if(size < sizeof(MemoryChunk)) {
+        first = 0;
+    }
+    else {
+        first = (MemoryChunk*)start;
 
-		first -> allocated = false;
-		first -> prev = 0;
-		first -> next = 0;
-		first -> size = size - sizeof(MemoryChunk);
-	}
+        first -> allocated = false;
+        first -> prev = 0;
+        first -> next = 0;
+        first -> size = size - sizeof(MemoryChunk);
+    }
 }
 
 MemMgr::~MemMgr()
 {
-	if(activeMemMgr == this)
-		activeMemMgr = 0;
+    if(activeMemMgr == this)
+        activeMemMgr = 0;
 }
 
 void* MemMgr::malloc(size_t size)
 {
-	MemoryChunk *result = 0;
+    MemoryChunk *result = 0;
 
-	for(MemoryChunk* chunk = first; chunk != 0 && result == 0; chunk = chunk->next)
-		if(chunk->size > size && !chunk->allocated)
-			result = chunk;
+    for(MemoryChunk* chunk = first; chunk != 0 && result == 0; chunk = chunk->next)
+        if(chunk->size > size && !chunk->allocated)
+            result = chunk;
 
-	if(result == 0)
-		return 0;
+    if(result == 0)
+        return 0;
 
-	if(result->size >= size + sizeof(MemoryChunk) + 1) {
-		MemoryChunk* temp = (MemoryChunk*)((size_t)result + sizeof(MemoryChunk) + size);
+    if(result->size >= size + sizeof(MemoryChunk) + 1) {
+        MemoryChunk* temp = (MemoryChunk*)((size_t)result + sizeof(MemoryChunk) + size);
 
-		temp->allocated = false;
-		temp->size = result->size - size - sizeof(MemoryChunk);
-		temp->prev = result;
-		temp->next = result->next;
-		if(temp->next != 0)
-			temp->next->prev = temp;
+        temp->allocated = false;
+        temp->size = result->size - size - sizeof(MemoryChunk);
+        temp->prev = result;
+        temp->next = result->next;
+        if(temp->next != 0)
+            temp->next->prev = temp;
 
-		result->size = size;
-		result->next = temp;
-	}
+        result->size = size;
+        result->next = temp;
+    }
 
-	result->allocated = true;
-	return (void*)(((size_t)result) + sizeof(MemoryChunk));
+    result->allocated = true;
+    return (void*)(((size_t)result) + sizeof(MemoryChunk));
 }
 
 void MemMgr::free(void* ptr)
 {
-	MemoryChunk* chunk = (MemoryChunk*)((size_t)ptr - sizeof(MemoryChunk));
+    MemoryChunk* chunk = (MemoryChunk*)((size_t)ptr - sizeof(MemoryChunk));
 
-	chunk -> allocated = false;
+    chunk -> allocated = false;
 
-	if(chunk->prev != 0 && chunk->prev->allocated) {
-		chunk->prev->next = chunk->next;
-		chunk->prev->size += chunk->size + sizeof(MemoryChunk);
+    if(chunk->prev != 0 && chunk->prev->allocated) {
+        chunk->prev->next = chunk->next;
+        chunk->prev->size += chunk->size + sizeof(MemoryChunk);
 
-		if(chunk->next != 0)
-			chunk->next->prev = chunk->prev;
+        if(chunk->next != 0)
+            chunk->next->prev = chunk->prev;
 
-		chunk = chunk->prev;
-	}
+        chunk = chunk->prev;
+    }
 
-	if(chunk->next != 0 && !chunk->next->allocated) {
-		chunk->size += chunk->next->size + sizeof(MemoryChunk);
-		chunk->next = chunk->next->next;
+    if(chunk->next != 0 && !chunk->next->allocated) {
+        chunk->size += chunk->next->size + sizeof(MemoryChunk);
+        chunk->next = chunk->next->next;
 
-		if(chunk->next != 0)
-			chunk->next->prev = chunk;
-	}
+        if(chunk->next != 0)
+            chunk->next->prev = chunk;
+    }
 }
