fc85c5a127354dc2653295594cbbe002b9888c09
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2019-07-16 01:51:46 +0200

    Create printf.h

---
+++ lib/libastd/printf.h
@@ -0,0 +1,46 @@
+/*
+ * This file is part of Asea OS.
+ * Copyright (C) 2019 Ivan Kmeťo
+ *
+ * Asea OS is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Asea OS is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Asea OS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ASEA__LIB__ASTD__PRINTF_H
+#define __ASEA__LIB__ASTD__PRINTF_H
+
+#include <common/types.h>
+
+void _sysprintf(char*, asea::common::uint8_t, asea::common::uint8_t);
+
+
+//
+static void printf(char* str)
+{
+    _sysprintf(str, 0x00, 0x0F);
+}
+
+//
+static void printf(char* str, asea::common::uint8_t forecolor)
+{
+    _sysprintf(str, 0x00, forecolor);
+}
+
+//
+static void printf(char* str, asea::common::uint8_t bgcolor, asea::common::uint8_t forecolor)
+{
+    _sysprintf(str, bgcolor, forecolor);
+}
+
+
+#endif
