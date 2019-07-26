755570f9336ec37b926c4407cb57ce16fd611f41
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2019-07-26 23:42:05 +0200

    Create asea.h

---
+++ alinc/asea.h
@@ -0,0 +1,35 @@
+/*
+ * This file is part of Asea OS.
+ * Copyright (C) 2019 Ivan Kmeťo
+ *
+ * Asea OS is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Asea OS is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Asea OS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ASEA__ASEA_H
+#define __ASEA__ASEA_H
+
+
+#include "system/headers/sysmsgs.h"
+#include "system/headers/sysnfo.h"
+#include "system/headers/verdef.h"
+
+#include "system/cursor.h"
+#include "system/syscalls.h"
+
+
+using namespace asea::system;
+using namespace asea::system::headers;
+
+
+#endif
