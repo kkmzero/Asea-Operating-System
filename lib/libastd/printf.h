a5fbaeb06a82d3333ab3b9e29381290b26f192a7
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2020-06-28 19:25:55 +0200

    Update printf.h

--- lib/libastd/printf.h
+++ lib/libastd/printf.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of Asea OS.
- * Copyright (C) 2019 Ivan Kmeťo
+ * Copyright (C) 2019 - 2020 Ivan Kmeťo
  *
  * Asea OS is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
